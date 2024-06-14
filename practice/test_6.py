1171d67da68204f809664140b37887c7bfa90c2f
lalibrarianne <167975710+lalibrarianne@users.noreply.github.com>
2024-06-14 14:20:24 +0300

    test_6

--- practice/test_6.py
+++ practice/test_6.py
@@ -14,30 +14,22 @@ class TestRegForm_negative(unittest.TestCase):
             browser.get(link)
             time.sleep(5)
 
-            container = browser.find_element(By.ID, "shadow-host")
+            container = browser.find_element(By.CSS_SELECTOR, "#shadow-host")
             container_shadow = container.shadow_root
-            inputs = container_shadow.find_elements(By.CSS_SELECTOR, "input")
+            container2 = container_shadow.find_element(By.CSS_SELECTOR,'div[id="shadow-host-two"]')
+            container_shadow2 = container2.shadow_root
+            inputs = container_shadow2.find_elements(By.CSS_SELECTOR, "input")
 
-            # первое обязательное поле - имя:
-
-            input_field = inputs[0]
-            input_field.send_keys("Homa")
+            input_field1 = inputs[0]
+            input_field1.send_keys("Homa")
+            
+            input_field2 = inputs[1]
+            input_field2.send_keys("Homa@example.mail.ru")
             time.sleep(3)
-            input_field = inputs[1]
-            input_field.send_keys("Homa@example.mail.ru")
+            
+            browser.execute_script("arguments[0].value = 'Alisa'", input_field1)
+            browser.execute_script("arguments[0].value = 'Alisa@no.com'", input_field2)
 
-
-
-            #browser.find_element(By.ID,'name').send_keys('Homa')
-            #browser.find_element(By.ID,'email').send_keys('Homa@example.mail.ru')
-            #time.sleep(3)
-            browser.find_element(By.CSS_SELECTOR,'button[type="submit"]').click()
-           
-            # Очистка поля ввода
-            browser.execute_script("arguments[0].value = 'Alisa'", input_field)
-
-
-            # Для демонстрации
             time.sleep(2)
            
         finally:
